84e9b8bcff6df4a0e9e77f023239db004a4af577
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2025-06-02 01:09:44 +0200

    Player Added
    Basic horizontal movement
    +Box collider 2D
    +Rigidbody 2D
    (Player Material)
    +Player Movement V1
    ++Sprite

---
+++ Assets/Scripts/PlayerMovementV1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementV1 : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer rend;
+
+    private float horizontalValue;
+
+    private bool canMove;
+
+    void Start()
+    {
+        canMove = true;
+        rb = GetComponent<Rigidbody2D>();
+        rend = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+        horizontalValue = Input.GetAxis("Horizontal");
+
+    }
+    private void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+        rb.velocity = new Vector2(horizontalValue * moveSpeed * Time.deltaTime, rb.velocity.y);
+    }
+}
