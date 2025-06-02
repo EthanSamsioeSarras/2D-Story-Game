a19887471be0f496a2c8ccbb84865c755fb99612
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2025-06-02 12:55:49 +0200

    PauseMenu Options Methods added
    +OpenOptions();
    +CloseOption();
    +ApplyOptions();
    
    ApplyOptions currently just prints out a Debug.Log();

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenu;
+    public GameObject pauseMenuContainer;
+    public GameObject optionsMenuContainer;
     private bool isPaused;
+    private bool inOptions;
 
     private void Awake()
     {
         isPaused = false;
-        pauseMenu.SetActive(false);
+        inOptions = false;
+        pauseMenuContainer.SetActive(false);
+        optionsMenuContainer.SetActive(false);
     }
 
     void Update()
@@ -25,21 +29,46 @@ public class PauseMenu : MonoBehaviour
             {
                 Pause();
             }
+
+            if (inOptions)
+            {
+                CloseOptions();
+            }
+
         }
     }
 
     public void Pause()
     {
         isPaused = true;
-        pauseMenu.SetActive(true);
+        pauseMenuContainer.SetActive(true);
         Time.timeScale = 0.0f;
     }
 
     public void Resume()
     {
         isPaused = false;
-        pauseMenu.SetActive(false);
+        pauseMenuContainer.SetActive(false);
         Time.timeScale = 1.0f;
+    }
+
+    public void OpenOptions()
+    {
+        inOptions = true;
+        optionsMenuContainer.SetActive(true);
+        pauseMenuContainer.SetActive(false);
+    }
+
+    public void CloseOptions()
+    {
+        inOptions = false;
+        optionsMenuContainer.SetActive(false);
+        pauseMenuContainer.SetActive(true);
+    }
+
+    public void ApplyOptions()
+    {
+        Debug.Log("Saving Settings");
     }
 
 }
