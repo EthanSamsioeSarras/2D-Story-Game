a722f625050f245771a87418e2243cbb08f3fa83
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2025-06-03 22:31:14 +0200

    Load method created in FileDataHandler
    Gets path to directory,
    Loads in the sterilized data from the file,
    Deserializes the data

--- Assets/Scripts/DataPersistence/FileDataHandler.cs
+++ Assets/Scripts/DataPersistence/FileDataHandler.cs
@@ -17,7 +17,34 @@ public class FileDataHandler
 
     public GameData Load()
     {
+        //Using Path.Combine to account for different OS's having different path separators
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        GameData loadedData = null;
 
+        if (File.Exists(fullPath))
+        {
+            try
+            {
+                //Load the serialized data from the file
+                string dataToLoad = "";
+                using(FileStream stream = new FileStream(fullPath, FileMode.Open))
+                {
+                    using(StreamReader reader = new StreamReader(stream))
+                    {
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+
+                //Deserialize the data from Json back into C# object
+                loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error occured when trying to load data from file: " + fullPath + "\n" + e);
+            }
+        }
+        
+        return loadedData;
     }
 
     public void Save(GameData data)
