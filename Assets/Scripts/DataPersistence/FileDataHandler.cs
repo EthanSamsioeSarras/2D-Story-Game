84a305174df4e97163c3ae9ca355f9041c06bf5d
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2025-06-03 22:17:42 +0200

    Save method created in FileDataHandler
    Creates directory path,
    Creates the Json file,
    fills it with the saved data

--- Assets/Scripts/DataPersistence/FileDataHandler.cs
+++ Assets/Scripts/DataPersistence/FileDataHandler.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
-public class FileDataHandler : MonoBehaviour
+public class FileDataHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private string dataDirPath = "";
+    private string dataFileName = "";
+
+    public FileDataHandler(string dataDirPath, string dataFileName)
     {
-        
+        this.dataDirPath = dataDirPath;
+        this.dataFileName = dataFileName;
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameData Load()
     {
-        
+
     }
+
+    public void Save(GameData data)
+    {
+        //Using Path.Combine to account for different OS's having different path separators
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        try
+        {
+            //Create the diractory path if it doesn't exist
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+            //Serializing the C# game data object to Json
+            string dataToStore = JsonUtility.ToJson(data, true);
+
+            //Write the serialized data to the file
+            using(FileStream stream = new FileStream(fullPath, FileMode.Create))
+            {
+                using(StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(dataToStore);
+                }
+            }
+        }catch (Exception e)
+        {
+            Debug.LogError("Error occured when trying to save data to file: " + fullPath + "\n" + e);
+        }
+    }
+
 }
